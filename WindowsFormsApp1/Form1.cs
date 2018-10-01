92b636d246298306f7afa54687f3d43999915b77
SideBack_Tim <38149519+sideback909@users.noreply.github.com>
2018-10-01 23:40:50 +0700

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary2;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nominal = int.Parse(nilai.Text);
+
+            Class1 none = new Class1();
+            
+            lstHasil.Text = Convert.ToString(none.TerbilangIndo(nominal));
+        }
+    }
+}
